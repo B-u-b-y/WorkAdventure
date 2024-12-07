ba78d60c967493ad4ef8d55b845eac35814b2108
Riccardo Budaes <riccardo.budaestanar@edu.unito.it>
2024-12-07 19:18:57 +0100

    Created some meeting rooms

--- src/main.ts
+++ src/main.ts
@@ -9,34 +9,40 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA.player.tags);
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('clock').subscribe(closePopup);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        currentPopup = WA.ui.openPopup("clockPopup", "1.Seguire una lezione\n2.Valutare relatore o studenti", []);
-    })
+    WA.room.area.onEnter('info').subscribe(() => {
+        currentPopup = WA.ui.openPopup("infoPopup", "1.Seguire una lezione\n2.Avvicinarsi alla lavagna\n3.Valutare relatore o studenti", []);
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('info').subscribe(closePopup);
+
+    WA.room.area.onEnter('valuta').subscribe(() => valuta());
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e))
+    }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
 
-function closePopup(){
+function closePopup() {
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
+}
+
+function valuta() {
+    console.log('Apertura della pagina laterale...');
 }
 
 export {};
